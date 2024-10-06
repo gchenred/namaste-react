c578e61b0e1f6d0c7061f38e7d3004725f324c97
Govardhan Reddy <govardhanreddyc25@gmail.com>
2024-10-06 06:33:46 +0530

    episode 2

---
+++ Notes/Episode2/App.js
@@ -0,0 +1,4 @@
+// Parcel is Beast
+// npm init -  creates package.json file 
+// npm install -D parcel - installs parcel as DEV dependecy and package-lock.json and node_modules are created
+// npm parcel index.html - builds our app, cache and dist folders are created
