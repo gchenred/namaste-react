6ee7555cc4bb873a577bd40a81818ff72123cbe4
Govardhan Reddy <govardhanreddyc25@gmail.com>
2025-01-03 12:24:21 +0530

    search ep6

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,10 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [resList, setResList] = useState([]);
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [filteredList, setFilteredResList] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("");
+  console.log("Render");
 
   useEffect(() => {
     fetchData();
@@ -15,12 +18,15 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4092881&lng=78.5432644&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    setResList(
+    setRestaurantList(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredResList(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
-  return resList.length === 0 ? (
+  return restaurantList.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body-container">
@@ -28,17 +34,39 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            filteredList = resList.filter(
-              (restaurant) => restaurant.info.avgRating > 4
+            topRatedList = restaurantList.filter(
+              (restaurant) => restaurant.info.avgRating > 4.3
             );
-            setResList(filteredList);
+            setFilteredResList(topRatedList);
           }}
         >
           Top Restaurants
         </button>
+        <div className="search-container">
+          <input
+            className="search-input"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            className="search-btn"
+            onClick={() => {
+              filteredRestaurants = restaurantList.filter((restaurant) =>
+                restaurant?.info?.name
+                  ?.toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilteredResList(filteredRestaurants);
+            }}
+          >
+            Search
+          </button>
+        </div>
       </div>
       <div className="restuarant-list">
-        {resList.map((restaurant) => (
+        {filteredList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,30 @@
 import Logo from "../../Images/Landing/Logo.jpg";
+import { useState } from "react";
 
 const Header = () => {
-    return(
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src={Logo}></img>
-            </div>
-            <div className="nav-items-container">
-                <ul className="nav-items-list">
-                    <li className="nav-item">Home</li>
-                    <li className="nav-item">Cart</li>
-                    <li className="nav-item">Contact Us</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  const [btnName, setBtnName] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={Logo}></img>
+      </div>
+      <div className="nav-items-container">
+        <ul className="nav-items-list">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">Cart</li>
+          <li className="nav-item">Contact Us</li>
+          <button
+            className="nav-item-btn"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
